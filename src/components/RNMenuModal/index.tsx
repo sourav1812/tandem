44fb280c981914c32d27746b0eb279a18293f866
maheepDev <maheep.singh@geeky.dev>
2023-07-11 12:04:23 +0530

    comments resolved

--- src/components/RNMenuModal/index.tsx
+++ src/components/RNMenuModal/index.tsx
@@ -1,4 +1,4 @@
-import {View, StatusBar, Pressable} from 'react-native';
+import {View, Pressable} from 'react-native';
 import React from 'react';
 import {styles} from './styles';
 import RNTextComponent from '../RNTextComponent';
@@ -42,60 +42,3 @@ const RNMenuModal = ({visible, renderModal}: menuModalProps) => {
 };
 
 export default RNMenuModal;
-
-// import {View, StatusBar, Pressable} from 'react-native';
-// import React from 'react';
-// import {styles} from './styles';
-// import RNTextComponent from '../RNTextComponent';
-// import {verticalScale} from 'react-native-size-matters';
-// import Modal from 'react-native-modal';
-// import {checkIfTablet} from '../../hooks/isTabletHook';
-// import {menuModalProps} from './interface';
-// import Notes from '../../assets/svg/Pdf';
-// import Send from '../../assets/svg/Send';
-// import Delete from '../../assets/svg/Delete';
-// import themeColor from '../../theme/themeColor';
-
-// const RNMenuModal = ({visible, renderModal}: menuModalProps) => {
-//   let isTablet = checkIfTablet();
-//   return (
-//     <Modal
-//       isVisible={visible}
-//       style={styles.modal}
-//       backdropOpacity={0.2}
-//       onBackButtonPress={renderModal}
-//       onBackdropPress={renderModal}
-//       animationIn={'slideInRight'}
-//       animationOut={'slideOutRight'}
-//       animationOutTiming={600}
-//       animationInTiming={600}>
-//       <StatusBar
-//         translucent
-//         backgroundColor={'rgba(0, 0, 0, 0.2)'}
-//         hidden={false}
-//         showHideTransition={'slide'}
-//       />
-//       <View
-//         style={[styles.container, isTablet && {maxWidth: verticalScale(270)}]}>
-//         <Pressable style={styles.menu}>
-//           <RNTextComponent style={styles.text}>Share Pdf</RNTextComponent>
-//           <Notes />
-//         </Pressable>
-//         <Pressable style={styles.menu}>
-//           <RNTextComponent style={styles.text}>
-//             Invite to read on tandem
-//           </RNTextComponent>
-//           <Send />
-//         </Pressable>
-//         <Pressable style={styles.menu}>
-//           <RNTextComponent style={[styles.text, {color: themeColor.red}]}>
-//             Delete
-//           </RNTextComponent>
-//           <Delete />
-//         </Pressable>
-//       </View>
-//     </Modal>
-//   );
-// };
-
-// export default RNMenuModal;
