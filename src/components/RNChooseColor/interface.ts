dc3ddb1fa28430fd57648aad111b1ea94b7b79b1
maheepDev <maheep.singh@geeky.dev>
2023-08-07 17:05:06 +0530

    grid layout improved

--- src/components/RNChooseColor/interface.ts
+++ src/components/RNChooseColor/interface.ts
@@ -1,6 +1,9 @@
+import {StyleProp, ViewStyle} from 'react-native';
+
 export interface colorPaletteType {
   tooltipVisible: boolean;
   onTooltipClose: () => void;
+  customStyle?: StyleProp<ViewStyle>;
 }
 export interface StateObject {
   colorPalette: {firstColor: string; secondColor: string}[];
