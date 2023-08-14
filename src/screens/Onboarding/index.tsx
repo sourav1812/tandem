6b29efb65b2fe060eb1d6292888906fabfc2cc62
Simranjeet92singh <simranjeet.singh@geeky.dev>
2023-08-14 17:42:58 +0530

    reorganize onboarding screen to opt 1 layout

--- src/screens/Onboarding/index.tsx
+++ src/screens/Onboarding/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useCallback, useEffect, useRef, useState} from 'react';
-import {FlatList, View, Image, Dimensions} from 'react-native';
+import {FlatList, View, Dimensions, ImageBackground} from 'react-native';
 import {styles} from './styles';
 import RNScreenWrapper from '@tandem/components/RNScreenWrapper';
 import {ONBOARDING} from './interface';
@@ -41,7 +41,7 @@ const Onboarding = () => {
     },
     {
       id: 2,
-      description: translation('WITH_TANDEM_YOU_WILL_THE_POWER'),
+      description: translation('YOUR_CHILD_CAN_CHOOSE_FROM_A_VARIETY'),
       url: portrait
         ? require('@tandem/assets/png/onboarding3.png')
         : require('@tandem/assets/png/onboardingLandscape3.png'),
@@ -58,41 +58,17 @@ const Onboarding = () => {
   const renderBanner = useCallback(
     ({item}: {item: any}) => {
       return (
-        <View
-          style={{
-            backgroundColor: themeColor.white,
-            height: height,
-            width: width,
-            alignItems: 'center',
-          }}>
-          <Image
-            style={[
-              styles.img,
-              {
-                height: height / 1.8,
-                width: width - scale(30),
-                marginTop: isTablet ? scale(20) : scale(50),
-              },
-            ]}
-            source={item.url}
-            resizeMode="stretch"
-          />
-          <RNTextComponent isSemiBold style={styles.heading}>
-            {translation('WELCOME_TO_TANDEM')}
-          </RNTextComponent>
-          <RNTextComponent
-            style={[
-              styles.content,
-              isTablet && {paddingHorizontal: scale(width / 10)},
-            ]}>
-            {item.description}
-          </RNTextComponent>
-        </View>
-        // <ImageBackground
-        //   source={item.url}
-        //   resizeMode="cover"
-        //   style={styles.img}
-        // />
+        <ImageBackground
+          style={[
+            // styles.img,
+            {
+              height: height,
+              width: width,
+            },
+          ]}
+          source={item.url}
+          resizeMode="cover"
+        />
       );
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -144,36 +120,47 @@ const Onboarding = () => {
         }}
         extraData={portrait}
       />
-      <View
-        style={[
-          styles.footer,
-          isTablet && {paddingHorizontal: scale(80)},
-          {bottom: portrait ? verticalScale(50) : 0},
-        ]}>
-        <View style={styles.indicator}>
-          {Array.from({length: 3}, (_, i) => (
-            <View
-              key={i.toString()}
-              style={[
-                styles.inactive,
-                i === currentIndex && {
-                  width: isTablet ? scale(16) : scale(22),
-                  backgroundColor: themeColor.themeBlue,
-                },
-              ]}
-            />
-          ))}
+      <View style={[styles.footer, isTablet && {paddingHorizontal: scale(80)}]}>
+        <RNTextComponent isSemiBold style={styles.heading}>
+          {translation('WELCOME_TO_TANDEM')}
+        </RNTextComponent>
+        <RNTextComponent
+          style={[
+            styles.content,
+            {height: !portrait ? verticalScale(60) : verticalScale(80)},
+          ]}>
+          {onboardingList[currentIndex].description}
+        </RNTextComponent>
+        <View
+          style={{
+            height: !portrait ? verticalScale(50) : 'auto',
+            width: '100%',
+          }}>
+          <View style={styles.indicator}>
+            {Array.from({length: 3}, (_, i) => (
+              <View
+                key={i.toString()}
+                style={[
+                  styles.inactive,
+                  i === currentIndex && {
+                    width: isTablet ? scale(16) : scale(22),
+                    backgroundColor: themeColor.themeBlue,
+                  },
+                ]}
+              />
+            ))}
+          </View>
+          <RNButton
+            title={translation('GET_STARTED')}
+            onClick={() => {
+              nextPage();
+            }}
+            customStyle={[
+              styles.button,
+              isTablet && {width: scale(160), alignSelf: 'center'},
+            ]}
+          />
         </View>
-        <RNButton
-          title={translation('GET_STARTED')}
-          onClick={() => {
-            nextPage();
-          }}
-          customStyle={[
-            styles.button,
-            isTablet && {width: scale(160), alignSelf: 'center'},
-          ]}
-        />
       </View>
     </RNScreenWrapper>
   );
