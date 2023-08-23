28744eb7c6ffce346f5a25a950e1eb54c3acf085
Sahil Sharma <sahil.sharma@geeky.dev>
2023-08-23 11:51:52 +0530

    code refactoring

--- src/components/PageFlip/Project.tsx
+++ src/components/PageFlip/Project.tsx
@@ -17,27 +17,40 @@ import {
   useImage,
   rrect,
   rect,
+  SkFont,
+  SkImage,
+  SkRRect,
 } from '@shopify/react-native-skia';
 import {Dimensions, PixelRatio} from 'react-native';
 import React from 'react';
-
 import {pageCurl} from './pageCurl';
 import {verticalScale} from 'react-native-size-matters';
 
+interface ProjectProps {
+  text1: string;
+  text2: string;
+  pic1: any;
+  pic2: any;
+  setActiveIndex: React.Dispatch<React.SetStateAction<number>>;
+  activeIndex: number;
+}
+interface RenderSceneProps {
+  image: SkImage | null;
+  showBackdrop: boolean;
+  roundedRect: SkRRect;
+  sentences: string[];
+  font: SkFont | null;
+}
+
 const {width: wWidth, height: hHeight} = Dimensions.get('screen');
 const pd = PixelRatio.get();
-const height = hHeight;
-const outer = Skia.XYWHRect(0, 0, wWidth, height);
-const pad = 0;
+const outer = Skia.XYWHRect(0, 0, wWidth, hHeight);
 const cornerRadius = 0;
+const padding = verticalScale(50);
+const fontSize = verticalScale(16);
+const numberOfChars = Math.floor((wWidth * 1.5) / fontSize);
 
-const inner = Skia.RRectXY(
-  Skia.XYWHRect(pad, pad, wWidth - pad * 2, height - pad * 2),
-  cornerRadius,
-  cornerRadius,
-);
-
-const processSentences = (text: string, numberOfChars: number) => {
+const processSentences = (text: string) => {
   let maxCharsReached = 0;
   let wordsArray: string[] = [];
   const sentenceArray: string[] = [];
@@ -55,19 +68,20 @@ const processSentences = (text: string, numberOfChars: number) => {
       maxCharsReached += word.length;
     }
   });
+
   const sentence = wordsArray.join(' ');
   sentenceArray.push(sentence);
-  return sentenceArray;
+
+  return sentenceArray.reverse();
 };
 
-interface ProjectProps {
-  text1: string;
-  text2: string;
-  pic1: any;
-  pic2: any;
-  setActiveIndex: React.Dispatch<React.SetStateAction<number>>;
-  activeIndex: number;
-}
+const getRoundRect = (length: number) => {
+  return rrect(
+    rect(0, hHeight - padding - fontSize * length, wWidth, hHeight),
+    20,
+    20,
+  );
+};
 
 export const Project = ({
   text1,
@@ -76,21 +90,28 @@ export const Project = ({
   pic2,
   setActiveIndex,
 }: ProjectProps) => {
-  const {width} = outer;
+  const font = useFont(
+    require('@tandem/assets/fonts/Poppins-SemiBold.ttf'),
+    fontSize,
+  );
+
   const image1 = useImage(pic1);
   const image2 = useImage(pic2);
-  const origin = useValue(width);
-  const pointer = useValue(width);
+
+  const origin = useValue(wWidth);
+  const pointer = useValue(wWidth);
+
   const [showBackdrop, setShowBackdrop] = React.useState(false);
-  const fontSize = verticalScale(16);
-  const numberOfChars = Math.floor((width * 1.5) / fontSize);
   const [sentences1, setSentences1] = React.useState<string[]>([]);
   const [sentences2, setSentences2] = React.useState<string[]>([]);
-  const padding = verticalScale(50);
+
+  const roundedRect1 = getRoundRect(sentences1.length);
+  const roundedRect2 = getRoundRect(sentences2.length);
 
   React.useEffect(() => {
-    setSentences1(processSentences(text1, numberOfChars).reverse());
-    setSentences2(processSentences(text2, numberOfChars).reverse());
+    setSentences1(processSentences(text1));
+    setSentences2(processSentences(text2));
+
     if (!showBackdrop) {
       setShowBackdrop(true);
     }
@@ -107,7 +128,7 @@ export const Project = ({
     onEnd: ({x}) => {
       // ! conditional if we intend to go to next page the animation should be of page disappearing
       const turnpage = x < 100;
-      runTiming(pointer, turnpage ? -width : width, {
+      runTiming(pointer, turnpage ? -wWidth : wWidth, {
         duration: 1000,
         easing: Easing.in(Easing.sin),
       });
@@ -115,36 +136,22 @@ export const Project = ({
         setTimeout(() => {
           // TODO : DO SOMETHING ABOUT ASYNC BETWEEN THESE 2 UPDATES
           setActiveIndex(prev => prev + 1);
-          pointer.current = width;
+          pointer.current = wWidth;
         }, 1000);
       }
     },
   });
 
-  const font = useFont(
-    require('@tandem/assets/fonts/Poppins-SemiBold.ttf'),
-    fontSize,
-  );
-  const roundedRect1 = rrect(
-    rect(0, height - padding - fontSize * sentences1.length, wWidth, hHeight),
-    20,
-    20,
-  );
-  const roundedRect2 = rrect(
-    rect(0, height - padding - fontSize * sentences2.length, wWidth, hHeight),
-    20,
-    20,
-  );
   const uniforms = useComputedValue(() => {
     return {
       pointer: pointer.current * pd,
       origin: origin.current * pd,
       resolution: [outer.width * pd, outer.height * pd],
       container: [
-        inner.rect.x,
-        inner.rect.y,
-        inner.rect.x + inner.rect.width,
-        inner.rect.y + inner.rect.height,
+        outer.x,
+        outer.y,
+        outer.x + outer.width,
+        outer.y + outer.height,
       ].map(v => v * pd),
       cornerRadius: cornerRadius * pd,
     };
@@ -157,23 +164,13 @@ export const Project = ({
         height: outer.height,
       }}
       onTouch={onTouch}>
-      <Group clip={inner}>
-        <Image image={image2} rect={inner.rect} fit="cover" />
-        {showBackdrop && (
-          <BackdropBlur blur={8} clip={roundedRect2}>
-            <Fill color="rgba(255, 255, 255, 0.438)" />
-          </BackdropBlur>
-        )}
-        {sentences2.map((sentence, index) => (
-          <Text
-            key={index.toString()}
-            x={width / 2 - ((sentence.length / numberOfChars) * width) / 2.5}
-            y={height - padding / 2 - fontSize * index} //  number of line the text is on
-            text={sentence}
-            font={font}
-          />
-        ))}
-      </Group>
+      <RenderScene
+        image={image2}
+        roundedRect={roundedRect2}
+        sentences={sentences2}
+        showBackdrop={showBackdrop}
+        font={font}
+      />
       <Group transform={[{scale: 1 / pd}]}>
         <Group
           layer={
@@ -181,25 +178,44 @@ export const Project = ({
               <RuntimeShader source={pageCurl} uniforms={uniforms} />
             </Paint>
           }
-          transform={[{scale: pd}]}
-          clip={inner}>
-          <Image image={image1} rect={inner.rect} fit="cover" />
-          {showBackdrop && (
-            <BackdropBlur blur={8} clip={roundedRect1}>
-              <Fill color="rgba(255, 255, 255, 0.438)" />
-            </BackdropBlur>
-          )}
-          {sentences1.map((sentence, index) => (
-            <Text
-              key={index.toString()}
-              x={width / 2 - ((sentence.length / numberOfChars) * width) / 2.5}
-              y={height - padding / 2 - fontSize * index} //  number of line the text is on
-              text={sentence}
-              font={font}
-            />
-          ))}
+          transform={[{scale: pd}]}>
+          <RenderScene
+            image={image1}
+            roundedRect={roundedRect1}
+            sentences={sentences1}
+            showBackdrop={showBackdrop}
+            font={font}
+          />
         </Group>
       </Group>
     </Canvas>
+  );
+};
+
+const RenderScene = ({
+  image,
+  showBackdrop,
+  roundedRect,
+  sentences,
+  font,
+}: RenderSceneProps) => {
+  return (
+    <Group>
+      <Image image={image} rect={outer} fit="cover" />
+      {showBackdrop && (
+        <BackdropBlur blur={8} clip={roundedRect}>
+          <Fill color="rgba(255, 255, 255, 0.438)" />
+        </BackdropBlur>
+      )}
+      {sentences.map((sentence, index) => (
+        <Text
+          key={index.toString()}
+          x={wWidth / 2 - ((sentence.length / numberOfChars) * wWidth) / 2.5}
+          y={hHeight - padding / 2 - fontSize * index} //  number of line the text is on
+          text={sentence}
+          font={font}
+        />
+      ))}
+    </Group>
   );
 };
