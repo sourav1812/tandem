e82b36df270b804c96ed68026036d997e30e014a
Sahil Sharma <sahil.sharma@geeky.dev>
2023-09-01 12:20:51 +0530

    german langauage

--- src/constants/lang/i18n.tsx
+++ src/constants/lang/i18n.tsx
@@ -1,6 +1,6 @@
 import en from '@tandem/constants/lang/en.json';
 import fr from '@tandem/constants/lang/fr.json';
-import nl from '@tandem/constants/lang/nl.json';
+import de from '@tandem/constants/lang/de.json';
 import it from '@tandem/constants/lang/it.json';
 
 import {I18n} from 'i18n-js';
@@ -8,5 +8,5 @@ import {I18n} from 'i18n-js';
 const i18n = new I18n(en);
 i18n.defaultLocale = 'en';
 i18n.locale = 'en';
-i18n.translations = {en, fr, nl, it};
+i18n.translations = {en, fr, de, it};
 export default i18n;
