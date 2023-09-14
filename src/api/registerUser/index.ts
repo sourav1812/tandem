fa674af02fca51ca4beae5de4498e53465456ddc
Simranjeet92singh <simranjeet.singh@geeky.dev>
2023-09-14 17:26:06 +0530

    user registration api modified, tooltip bug fixes

--- src/api/registerUser/index.ts
+++ src/api/registerUser/index.ts
@@ -2,11 +2,11 @@ import {post} from '@tandem/api';
 import {RegisterUser} from './interface';
 import {API} from '@tandem/constants/api';
 
-export default async ({email, name, password}: RegisterUser) => {
+export default async ({email, firstName, lastName, password}: RegisterUser) => {
   try {
     const result = await post({
       path: API.REGISTER_USER,
-      data: {email, name, password},
+      data: {email, firstName, lastName, password},
     });
     return result;
   } catch (error) {
