f2d350262384ea5ae54b8c1372c4cd65f7f15943
Sahil <sahil.sharma@geeky.dev>
2024-03-11 14:00:28 +0530

    translation for about us and faqs

--- src/screens/HelpCenter/index.tsx
+++ src/screens/HelpCenter/index.tsx
@@ -175,48 +175,48 @@ const FAQScreen = () => {
   return (
     <>
       <ExpandDetails
-        title="What is TANDEM?"
-        text="TANDEM is an app that allows you and your child to generate and read stories together, and to share them with others. It’s AI powered and draws on cutting-edge developmental science, designed to help your child to thrive, and to help to build early relationships."
+        title={translation('FAQ_QA.Q1')}
+        text={translation('FAQ_QA.A1')}
       />
       <ExpandDetails
-        title="What age of children is TANDEM designed for?"
-        text="Tandem has been designed with children aged from 2-5 years in mind, but we think that those older and younger will love it too. The app adapts to different reading ages and developmental stages to try to make sure that it’s always pitched at the right level."
+        title={translation('FAQ_QA.Q2')}
+        text={translation('FAQ_QA.A2')}
       />
       <ExpandDetails
-        title="How does TANDEM keep my, and my child’s, data private and safe?"
-        text="Keeping your data secure, and being clear about what data we collect, are really important to us. We follow strict data protection standards which includes… [KEV to add here?]. You can read more about our approach to data protection here: [link to privacy policy]"
+        title={translation('FAQ_QA.Q3')}
+        text={translation('FAQ_QA.A3')}
       />
       <ExpandDetails
-        title="What data do you collect, and why?"
-        text="TANDEM, like most apps, collects data on your interactions with the app. We also collect data from your child’s interactions with the app. These include things like how often you use Tandem, how long you spend doing different tasks like creating stories and reading together, and how many stories you create. We are also planning to use audio and eventually video data from your time reading together to track things like your child’s reading level, and the ‘serve and return’ back-forth turn taking in your reading together. Before we introduce this feature we’ll ask for your explicit permission though."
+        title={translation('FAQ_QA.Q4')}
+        text={translation('FAQ_QA.A4')}
       />
       <ExpandDetails
-        title="Can I track my child's progress and activities through TANDEM?"
-        text="Not yet, but we’re working on this. We’re planning to build in both a survey that you can complete to track your child’s development, and - eventually - automated tracking of your child’s development. When we introduce these we’ll tell you more about them if you choose to opt into them."
+        title={translation('FAQ_QA.Q5')}
+        text={translation('FAQ_QA.A5')}
       />
       <ExpandDetails
-        title="How does TANDEM generate stories?"
-        text="You can think of it a bit like a set of AI powered story dice. When your child uses TANDEM they create stories by choosing from a set of characters, ideas, things and types of stories, and these are fed into our story engine, which then uses one of several AI large language models to create a story that we then check and improve on. Eventually, we’ll also allow these stories to link to each other, allowing the AI to make engaging, fun and interesting stories that both you and your child want to read. We’re also working on adding in sound effects too."
+        title={translation('FAQ_QA.Q6')}
+        text={translation('FAQ_QA.A6')}
       />
       <ExpandDetails
-        title="Is the content in TANDEM educational?"
-        text="Yes, but we’re more interested in you and your child having fun together. There is a wealth of evidence that shared attention and interaction in the early years are the ‘magic ingredient’ for good child development. So although the content we create will help to promote early learning and literacy, we’re really trying to help to support relationships through you having fun reading and laughing together. You can learn more about early interaction here."
+        title={translation('FAQ_QA.Q7')}
+        text={translation('FAQ_QA.A7')}
       />
       <ExpandDetails
-        title="What AI is in the ‘backend’ of the app?"
-        text="We’re currently testing a few different AI engines to power TANDEM, including those from OpenAI (GPT4 and DALLE 3) and those from Anthropic (Pi). Eventually, we will run our own servers, but for now our ‘Reasoning engine - Velo’ - the brains behind TANDEM, which checks all the content and helps to make sure it’s good quality - works with external APIs. You can read more about Velo here [link to blog-tbc]."
+        title={translation('FAQ_QA.Q8')}
+        text={translation('FAQ_QA.A8')}
       />
       <ExpandDetails
-        title="Do I have to pay to use TANDEM?"
-        text="No, at least not at the moment. For now, while we’re building and testing, everything is free. You can use TANDEM as much as you want. However, because every story creation costs money because of the computing power needed to make and to check it, eventually we will need to build a subscription model. But we’re very keen that those who don’t have many books at home are able to have access to TANDEM, so we’re looking at a number of business models to allow this."
+        title={translation('FAQ_QA.Q9')}
+        text={translation('FAQ_QA.A9')}
       />
       <ExpandDetails
-        title="How can I leave and delete all my information?"
-        text="We hope you don’t want to leave, but you can find the option to delete your account, and all the information we hold in it, in the settings page of the app."
+        title={translation('FAQ_QA.Q10')}
+        text={translation('FAQ_QA.A10')}
       />
       <ExpandDetails
-        title="What are the technical requirements for using TANDEM?"
-        text="Tandem is compatible with most modern smartphones and tablets. Check the app’s download page in the app store for specific operating system requirements."
+        title={translation('FAQ_QA.Q11')}
+        text={translation('FAQ_QA.A11')}
       />
     </>
   );
