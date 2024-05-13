971a2732e41703a6ad624c8a6c89bf5ee31be1c6
M2 <m2@192.168.1.12>
2024-05-13 16:15:31 +0530

    added turkish lang

--- src/constants/lang/i18n.tsx
+++ src/constants/lang/i18n.tsx
@@ -5,11 +5,12 @@ import it from '@tandem/constants/lang/it.json';
 import po from '@tandem/constants/lang/po.json';
 import ur from '@tandem/constants/lang/ur.json';
 import pu from '@tandem/constants/lang/pu.json';
+import tr from '@tandem/constants/lang/tr.json';
 
 import {I18n} from 'i18n-js';
 
 const i18n = new I18n(en);
 i18n.defaultLocale = 'en';
 i18n.locale = 'en';
-i18n.translations = {en, fr, de, it, po, ur, pu};
+i18n.translations = {en, fr, de, it, po, ur, pu, tr};
 export default i18n;
