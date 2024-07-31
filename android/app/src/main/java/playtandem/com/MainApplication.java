803286e35e2644dc887e7c0e009700dcdc26b1e7
Sahil <sahil.sharma@geeky.dev>
2024-07-31 13:31:42 +0530

    stable code

--- android/app/src/main/java/playtandem/com/MainApplication.java
+++ android/app/src/main/java/playtandem/com/MainApplication.java
@@ -14,6 +14,13 @@ import com.facebook.soloader.SoLoader;
 import java.util.List;
 import org.wonday.orientation.OrientationActivityLifecycle;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import androidx.annotation.Nullable;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -52,7 +59,14 @@ public class MainApplication extends Application implements ReactApplication {
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
   }
-
+  @Override
+  public Intent registerReceiver(@Nullable BroadcastReceiver receiver, IntentFilter filter) {
+      if (Build.VERSION.SDK_INT >= 34 && getApplicationInfo().targetSdkVersion >= 34) {
+          return super.registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED);
+      } else {
+          return super.registerReceiver(receiver, filter);
+      }
+  }
   @Override
   public void onCreate() {
     registerActivityLifecycleCallbacks(OrientationActivityLifecycle.getInstance());
