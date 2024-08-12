b1b6fbfc05d93979f172912c8e6e496b10e95eb2
Sahil <sahil.sharma@geeky.dev>
2024-08-12 13:44:08 +0530

    optimised product fetching

--- src/functions/revenueCat/index.ts
+++ src/functions/revenueCat/index.ts
@@ -1,9 +1,7 @@
 import {REVENUE_CAT_KEYS} from '@tandem/constants/api';
-import {getValueFromKey, storeKey} from '@tandem/helpers/encryptedStorage';
 import {Platform} from 'react-native';
 import Purchases, {
   LOG_LEVEL,
-  PRODUCT_CATEGORY,
   PurchasesStoreProduct,
 } from 'react-native-purchases';
 
@@ -19,30 +17,23 @@ export const initialiseRevenueCat = async (appUserID: string) => {
       apiKey: REVENUE_CAT_KEYS.android,
       appUserID,
     });
-
-    try {
-      const offerings = await Purchases.getOfferings();
-      if (offerings.current !== null) {
-        console.log('@@@@@@@', JSON.stringify(offerings));
-        // Display current offering with offerings.current
-        const productSKUs = offerings.current.availablePackages.map(
-          item => item.product.identifier,
-        );
-        storeKey('productSKUs', productSKUs);
-      }
-    } catch (e) {
-      console.log('error in offering list,', e);
-    }
   }
 };
 
 export const getRevenueCatProducts = async () => {
-  const productSKUs = getValueFromKey('productSKUs');
-  const products = await Purchases.getProducts(
-    productSKUs,
-    PRODUCT_CATEGORY.NON_SUBSCRIPTION,
-  );
-  return products;
+  try {
+    const offerings = await Purchases.getOfferings();
+    if (offerings.current !== null) {
+      console.log('@@@@@@@', JSON.stringify(offerings));
+      // Display current offering with offerings.current
+      const products = offerings.current.availablePackages.map(
+        item => item.product,
+      );
+      return products;
+    }
+  } catch (e) {
+    console.log('error in offering list,', e);
+  }
 };
 
 export const makePurchase = async (product: PurchasesStoreProduct) => {
